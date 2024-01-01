370b9a16ed984168f27962082e85caa467174e61
adhithya-bolishetti <adhithya.bolishetty@gmail.com>
2024-01-01 17:31:37 +0530

    Dashboard Improvements

--- script/index.js
+++ script/index.js
@@ -2,10 +2,18 @@ function signup()
 {
     document.getElementById("email").removeAttribute("required");
     document.getElementById("password").removeAttribute("required");
-    //removeattribute
     window.location.href = "./signup.html";
 }
 function login()
 {
+    let email =document.getElementById("email");
+    let password =document.getElementById("password");
+    email.setAttribute("required","");
+    password.setAttribute("required","");
+    if(email.value == "" || password.value == "")
+    {
+        alert("Please fill all the fields");
+        return;
+    }
     window.location.href = "./dashboard.html";
 }
